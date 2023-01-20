a0cb828cef43e963a6c58c622b966b1f24cc4e2d
jshenrique13 <pinheiro012345@gmail.com>
2023-01-19 23:54:01 -0300

    first

---
+++ teste.py
@@ -0,0 +1,37 @@
+import os
+from pikepdf import Pdf
+import aspose.words as aw
+
+
+# the target PDF document to split
+filename = "cheque.pdf"
+
+# load the PDF file
+pdf = Pdf.open(filename)
+p = 1 
+
+ 
+for n, page in enumerate(pdf.pages):
+
+    novo = Pdf.new()
+    novo.pages.append(page)
+    
+    name, ext = os.path.splitext(filename)
+    nome_arq = f"{name}-{p}.pdf"
+    novo.save(nome_arq)
+    doc = aw.Document(nome_arq)
+    doc.save(f"{name}-{p}.txt")
+    text = open(f"{name}-{p}.txt", "r", encoding="UTF-8")
+    nome = text.readlines()[5][:35]
+    nomef = ''
+    for n in nome: 
+        if not n.isnumeric() :
+            nomef += n
+    nomef = nomef.strip()        
+    try:        
+        os.rename(nome_arq, f'{nomef}.pdf')
+    except FileExistsError:
+        os.rename(nome_arq, f'{nomef}{p}.pdf')
+    p+=1
+
+
